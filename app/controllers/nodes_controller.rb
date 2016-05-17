d28292894b5304b50338b39e63d6d4d1f4d84a51
aribrenner <aribrenner@gmail.com>
2016-05-16 20:36:25 -0400

    add NodesController actions

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,12 +1,41 @@
 class NodesController < ApplicationController
 
+  before_action :find_by_short_url, only: [:find_redirect, :show]
+
   def find_redirect
-    node = Node.find_by_short_url(params[:short_url])
-    if node
-      redirect_to node.redirect_url
+    redirect_to @node.redirect_url
+  end
+
+  def new
+    @node = Node.new
+  end
+
+  def create
+    node = Node.new(create_params)
+
+    if node.save
+      redirect_to node_path(node.short_url)
     else
-      raise ActionController::RoutingError.new(:short_url)
+      render :new
     end
+  end
+
+  def show
+  end
+
+  def index
+    @nodes = Node.all
+  end
+
+  private
+
+  def create_params
+    params.require(:node).permit(:redirect_url)
+  end
+
+  def find_by_short_url
+    @node = Node.find_by_short_url(params[:short_url])
+    raise ActionController::RoutingError.new(:short_url) unless @node
   end
 
 end
--- app/models/node.rb
+++ app/models/node.rb
@@ -3,4 +3,27 @@ class Node < ActiveRecord::Base
   validates_presence_of :short_url, :redirect_url
   validates_uniqueness_of :short_url
 
+  before_validation :set_short_url, on: :create
+
+  SHORT_URL_LENGTH = 6
+
+  private
+
+  def set_short_url
+    random_string = ''
+    SHORT_URL_LENGTH.times do
+      random_string += (accepted_chars.sample)
+    end
+
+    set_short_url if Node.find_by_short_url(random_string)
+
+    self.short_url = random_string
+  end
+
+  def accepted_chars
+    ('a'..'z').to_a +
+    ('z'..'Z').to_a +
+    ('0'..'9').to_a
+  end
+
 end
