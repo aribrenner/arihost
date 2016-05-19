19e44d1b85a0ce17a76c58916e183d42c86cb34c
aribrenner <aribrenner@gmail.com>
2016-05-18 22:17:36 -0400

    create users controller and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(create_params)
+    if @user.save
+      sign_in(@user)
+      redirect_to nodes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   resources :nodes, param: :short_url
-
-
   get ':short_url', to: 'nodes#find_redirect'
+
+  resources :users, param: :username
 
 end
