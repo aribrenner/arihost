398af57476f3aa7ba4f74097d42f50fe58171ca8
aribrenner <aribrenner@gmail.com>
2016-05-18 23:09:27 -0400

    current_user in application.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   def sign_in(user)
     session[:session_token] = SecureRandom.urlsafe_base64(16)
     user.update!(session_token: session[:session_token])
