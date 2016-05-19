0f20631ebbb3da6ef1f5251065445be53672dd26
aribrenner <aribrenner@gmail.com>
2016-05-18 22:35:00 -0400

    add sign_in and sign_out methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def sign_in(user)
+    session[:session_token] = SecureRandom.urlsafe_base64(16)
+    user.update!(session_token: session[:session_token])
+  end
+
+  def sign_out
+    return unless current_user
+    current_user.update!(session_token: nil)
+    session[:session_token] = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
 end
