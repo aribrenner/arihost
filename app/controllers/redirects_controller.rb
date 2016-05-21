eff0b62bbb22e8844a473a3f81b26ab972a94fc4
aribrenner <aribrenner@gmail.com>
2016-05-21 00:33:15 -0400

    create hit on redirect

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,9 +1,18 @@
 class RedirectsController < ApplicationController
 
   def find_redirect
-    node = Node.find_by_short_url(params[:short_url])
-    raise ActionController::RoutingError.new(:short_url) unless node
-    redirect_to node.redirect_url
+    @node = Node.find_by_short_url(params[:short_url])
+    raise ActionController::RoutingError.new(:short_url) unless @node
+    create_hit
+    redirect_to @node.redirect_url
+  end
+
+  def create_hit
+    Hit.create(
+      ip: request.remote_ip,
+      device: request.user_agent,
+      node: @node
+    )
   end
 
 end
