926380911381b963c5c513ebdb625d2fd8d0479a
aribrenner <aribrenner@gmail.com>
2016-05-25 19:24:49 -0400

    include hash when creating new node

---
+++ app/assets/javascripts/new_node.js
@@ -0,0 +1,8 @@
+$(function(){
+  var redirectInput = document.getElementById('node_redirect_url');
+  var hash = window.location.hash;
+
+  if (hash && redirectInput) {
+    redirectInput.value += hash;
+  }
+})
