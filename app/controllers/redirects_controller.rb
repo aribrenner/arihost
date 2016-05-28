b5907022c0e95a2a0798b99f19ca0cbf7f801241
aribrenner <aribrenner@gmail.com>
2016-05-28 14:26:45 -0400

    only ignore hit if same ip

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -23,6 +23,10 @@ class RedirectsController < ApplicationController
 
   private
 
+  attr_reader :last_hit
+
+  delegate :remote_ip, :user_agent, to: :request
+
   def find_hitable
     @hitable = @type.find_by_short_url(params[:short_url])
   end
@@ -30,16 +34,25 @@ class RedirectsController < ApplicationController
   def create_hit
     return if ignore_hit?
     Hit.create(
-      ip: request.remote_ip,
-      device: request.user_agent,
+      ip: remote_ip,
+      device: user_agent,
       hitable: @hitable,
       hitable_type: @type.name
     )
   end
 
   def ignore_hit?
-    last_hit = @hitable.hits.last
-    last_hit && (Time.now - last_hit.created_at) < Hit::IGNORE_THRESHOLD
+    @last_hit = @hitable.hits.last
+    return false unless last_hit && last_hit.ip.present?
+    under_threshold? && same_ip?
+  end
+
+  def under_threshold?
+    Time.now - last_hit.created_at < Hit::IGNORE_THRESHOLD
+  end
+
+  def same_ip?
+    last_hit.ip == remote_ip
   end
 
 end
