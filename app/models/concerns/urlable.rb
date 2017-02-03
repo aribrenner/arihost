945dcc455621f1c718ea26fa1662d535bedf2831
aribrenner <aribrenner@gmail.com>
2017-02-02 23:39:43 -0500

    simplify short_url methods

--- app/models/concerns/urlable.rb
+++ app/models/concerns/urlable.rb
@@ -3,14 +3,9 @@ module Urlable
   RESERVED = %w(http app api images)
 
   def set_short_url
-    random_string = ''
-    self.class::SHORT_URL_LENGTH.times do
-      random_string += (accepted_chars.sample)
-    end
-
-    set_short_url unless valid_url?(random_string)
-
-    self.short_url = random_string
+    str = self.class::SHORT_URL_LENGTH.times.map { random_char }.join
+    set_short_url unless valid_url?(str)
+    self.short_url = str
   end
 
   def accepted_chars
@@ -19,6 +14,10 @@ module Urlable
     ('B'..'Z').to_a +
     ('0'..'9').to_a -
     vowels - vowels.map(&:upcase)
+  end
+
+  def random_char
+    accepted_chars.sample
   end
 
   def valid_url?(random_string)
