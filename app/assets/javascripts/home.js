d1534df95ec242261be39fd05eb4f3587d8c7d04
aribrenner <aribrenner@gmail.com>
2017-03-08 00:17:16 -0500

    example ari.host prefix

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,9 @@
+ariHost(function() {
+  var $toggle = $('.welcome-page .js-show-me')
+
+  if (!$toggle.length) { return }
+
+  $toggle.click(function(){
+    $('.js-example-urls').toggleClass('hidden')
+  })
+})
